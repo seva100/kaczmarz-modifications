a127ebb24553b2bdf1cae78753fc80077e7cd5ff
Artem Sevastopolsky <artem.sevastopolsky@gmail.com>
2017-12-19 23:26:25 +0300

    Added simple band matrix generator

--- generate_matrices.py
+++ generate_matrices.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy as sp
+import scipy.sparse
 
 
 def over_determined(n, m, ampl=50):
@@ -24,7 +26,7 @@ def rand_for_given_cond_number(n, m, cond=1, rank='full'):
 
     if rank == 'full':
         rank = min(n, m)
-    sing_vals = np.linspace(1.0, cond, rank)
+    sing_vals = np.linspace(1.0, cond, rank)[::-1]
     print('sing_vals:', sing_vals)
     if rank < min(n, m):
         sing_vals = np.concatenate([sing_vals, np.zeros(min(n, m) - rank)])
@@ -37,3 +39,17 @@ def rand_for_given_cond_number(n, m, cond=1, rank='full'):
     print('Sigma:', Sigma)
     mx = U @ Sigma @ V
     return mx
+
+
+def band_mx(n, m, half_band_size=1, ampl=50, return_sparse=True):
+    """We are considering that band is always of size (2 * half_band_size + 1) (so it is always odd).
+    """
+    diag_vals = np.random.randn(2 * half_band_size + 1) * ampl - ampl / 2
+    print(diag_vals)
+    diag_data = np.repeat(diag_vals[:, np.newaxis], min(n, m), axis=1)
+    print(diag_data)
+    print(np.arange(-half_band_size, half_band_size + 1, dtype=np.int64))
+    mx = sp.sparse.spdiags(diag_data, np.arange(-half_band_size, half_band_size + 1, dtype=np.int64), n, m)
+    if return_sparse:
+        return mx
+    return mx.toarray()
